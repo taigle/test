df8f37be9e7144d9045a1ffabc36756268a26210
Administrator <15994790912@163.com>
2017-03-12 19:15:02 +0800

    A要删除的代码2

--- TestJava1/src/txl/Test.java
+++ TestJava1/src/txl/Test.java
@@ -7,7 +7,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("B的代码1");
         System.out.println("A要修改");
-        System.out.println("A要添加的");
-        System.out.println("B要添加的代码");
     }
 }
